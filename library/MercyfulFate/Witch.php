4c6c13a498415bbe9b3b98474e2023c833e840f4
Jeremy Kendall <jeremy@jeremykendall.net>
2013-11-15 08:37:54 -0600

    Refactors Witch, removes Melissa subclass, Witch implements SplObserver

--- library/MercyfulFate/Witch.php
+++ library/MercyfulFate/Witch.php
@@ -2,8 +2,15 @@
 
 namespace MercyfulFate;
 
-class Witch
+class Witch implements \SplObserver
 {
+    /**
+     * Witch's name
+     *
+     * @var string
+     */
+    protected $name;
+
     /**
      * Has the witch been burned at the stake?
      *
@@ -11,9 +18,56 @@ class Witch
      */
     protected $burned;
 
-    public function __construct($burned = false)
+    /**
+     * Has the witch entered another life?
+     *
+     * @var bool
+     */
+    protected $enteredAnotherLife;
+
+    /**
+     * Public constructor
+     *
+     * @param string $name               Witch's name
+     * @param bool   $burned             True if witch has burned at stake
+     * @param bool   $enteredAnotherLife True if witch is dead
+     */
+    public function __construct($name, $burned = false, $enteredAnotherLife = false)
     {
+        $this->name = $name;
         $this->burned = $burned;
+        $this->enteredAnotherLife = $enteredAnotherLife;
+    }
+
+    /**
+     * If notified, Witch will burn and enter another life
+     *
+     * @param \SplSubject $subject
+     */
+    public function update(\SplSubject $subject)
+    {
+        $this->setBurned(true);
+        $this->setEnteredAnotherLife(true);
+    }
+
+    /**
+     * Get witch's name
+     *
+     * @return string Witch's name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set witch's name
+     *
+     * @param string $name Witch's name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
     }
 
     /**
@@ -34,4 +88,23 @@ class Witch
         $this->burned = $burned;
     }
 
+    /**
+     * Has the witch entered another life?
+     *
+     * @return bool True if the witch is dead
+     */
+    public function isEnteredAnotherLife()
+    {
+        return $this->enteredAnotherLife;
+    }
+
+    /**
+     * Set enteredAnotherLife
+     *
+     * @param bool $enteredAnotherLife True if the witch is dead
+     */
+    public function setEnteredAnotherLife($enteredAnotherLife)
+    {
+        $this->enteredAnotherLife = $enteredAnotherLife;
+    }
 }
